6f217269647f7df0fcb0b5c842e9dbd3afa94f40
spade729 <i-v-babin@yandex.ru>
2025-09-29 11:30:57 +0300

    ver1.02

--- src/dz03/ex02.py
+++ src/dz03/ex02.py
@@ -4,27 +4,21 @@
 значения между собой без использования других списков и картежей.
 """
 
-#def fm_swap(arr1: list, arr2: list):
-#    """FM."""
-    
-#    min_arr_len = len(arr1) if len(arr1) < len(arr2) else len(arr2)
-    
-#    for i in range(min_arr_len):
-#        arr1[i], arr2[i] = arr2[i], arr1[i]
-
-    
-
 arr1 = [ 1, 2, 3, 4, 5]
 arr2 = ['q', 'w', 'v', 'n', 'u', '5g', 'qwe', 'jkkkk']
 
 print('До:')
 print(arr1)
 print(arr2)
+print(id(arr1))
+print(id(arr2))
 
-arr1, arr2 = arr2, arr1
-
-#fm_swap(arr1, arr2)
+#arr1, arr2 = arr2, arr1
+#arr1[0:len(arr1)], arr2[0:len(arr2)] = arr2[0:len(arr2)], arr1[0:len(arr1)]
+arr1[:], arr2[:] = arr2[:], arr1[:]
 
 print('После:')
 print(arr1)
 print(arr2)
+print(id(arr1))
+print(id(arr2))
